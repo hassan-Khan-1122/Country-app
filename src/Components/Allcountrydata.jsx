fbad19b235598fb6b1f3e93a35cbd99c1077128b
Hassan Khan <167732464+hassan-Khan-1122@users.noreply.github.com>
2024-06-21 12:37:46 +0100

    first commit

--- src/Components/Allcountrydata.jsx
+++ src/Components/Allcountrydata.jsx
@@ -1,97 +1,134 @@
-
-import React, { useState } from 'react';
-import useCountries from '../assets/Api/useCountiresData';
+import React, { useState } from "react";
+import useCountries from "../assets/Api/useCountiresData";
 import { FaArrowUp } from "react-icons/fa";
 
 const Allcountrydata = () => {
-    const { countries } = useCountries(); 
-    const [searchQuery, setSearchQuery] = useState(""); 
+  const { countries } = useCountries();
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const handleSearchInputChange = (e) => {
-        setSearchQuery(e.target.value);
-    };
+  const handleSearchInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
 
-    const filteredCountries = countries.filter(country =>
-        country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
-    );
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
+  function scrolltrigger() {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
 
-    function scrolltrigger() {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    }
-
-    return (
-        <div className='mt-24'>
-            {/* Search */}
-            <div className="mb-10">
-                <form className="max-w-md mx-auto">
-                    <label
-                        htmlFor="default-search"
-                        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-                    >
-                        Search
-                    </label>
-                    <div className="relative">
-                        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-                            <svg
-                                className="w-4 h-4 text-gray-500 dark:text-gray-400"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 20 20"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-                                />
-                            </svg>
-                        </div>
-                        <input
-                            type="search"
-                            id="default-search"
-                            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                            placeholder="Search Mockups, Logos..."
-                            value={searchQuery}
-                            onChange={handleSearchInputChange}
-                            required
-                        />
-                        {/* No need for this button */}
-                    </div>
-                </form>
+  return (
+    <div className="mt-24 px-4 sm:px-6 lg:px-8">
+    
+      <div className="mb-10">
+        <form className="max-w-md mx-auto">
+          <label
+            htmlFor="default-search"
+            className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+          >
+            Search
+          </label>
+          <div className="relative">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 20 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                />
+              </svg>
             </div>
+            <input
+              type="search"
+              id="default-search"
+              className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="Search countries..."
+              value={searchQuery}
+              onChange={handleSearchInputChange}
+              required
+            />
+          </div>
+        </form>
+      </div>
 
-            <div className='flex flex-wrap gap-3 w-[900px] bg-[#f0f0f0] mx-auto justify-center items-start'>
-                {filteredCountries.map((country, index) => (
-                    <div key={index} className='w-[400px] h-[600px] bg-white p-3 m-2 hover:scale-[1.005] hover:shadow-2xl rounded-lg transition-all duration-300 ease-in-out'>
-                        <img src={country.flags.png} alt={country.name.common} className='w-full h-auto' />
-                        <p className='text-3xl text-[orange] text-center font-[700] my-2'>{country.name.common}</p>
-                        <div className='flex items-center justify-start px-3'>
-                            <p className='text-3xl text-[#616161] font-[700] my-2'>Capital : <span className='text-[25px] text-[#747474]'>{country.capital}</span></p>
-                        </div>
-                        <div className='flex items-center justify-start px-3'>
-                            <p className='text-3xl text-[#616161] font-[700] my-2'>Languages : <span className='text-[25px] text-[#747474]'>{country.languages && Object.values(country.languages).map((language, index) => (
-                                <span key={index} className='text-xl text-[20px] text-[#616161]'>{language} </span>
-                            ))}</span></p>
-                        </div>
-                        <div className='flex items-center justify-start px-3'>
-                            <p className='text-3xl text-[#616161] font-[700] my-2'>Population : <span className='text-[25px] text-[#747474]'>{country.population}</span></p>
-                        </div>
-                    </div>
-                ))}
-            </div>
-            <div>
-                <button className='bg-green-500 rounded fixed right-3 bottom-3'onClick={scrolltrigger} ><FaArrowUp size={40}/></button>
-            </div>
+      {filteredCountries.length === 0 ? (
+        <div className="text-center text-2xl text-gray-500">
+          No countries found.
         </div>
-    );
-}
+      ) : (
+        <div className="flex flex-wrap gap-4 justify-center items-start">
+          {filteredCountries.map((country, index) => (
+            <div
+              key={index}
+              className="w-full sm:w-[45%] md:w-[30%] lg:w-[23%] bg-white p-3 m-2 hover:scale-[1.005] hover:shadow-2xl rounded-lg transition-all duration-300 ease-in-out"
+            >
+              <img
+                src={country.flags.png}
+                alt={country.name.common}
+                className="w-full h-auto rounded-md"
+              />
+              <p className="text-3xl text-[orange] text-center font-[700] my-2">
+                {country.name.common}
+              </p>
+              <div className="px-3">
+                <p className="text-lg text-[#616161] font-[700] my-2">
+                  Capital:{" "}
+                  <span className="text-[#747474]">{country.capital}</span>
+                </p>
+              </div>
+              <div className="px-3">
+                <p className="text-lg text-[#616161] font-[700] my-2">
+                  Languages
+                  <span className="text-[#747474]">
+                    {country.languages &&
+                      Object.values(country.languages).map(
+                        (language, index) => (
+                          <span
+                            key={index}
+                            className="text-[20px] text-[#616161]"
+                          >
+                            {language}{" "}
+                          </span>
+                        )
+                      )}
+                  </span>
+                </p>
+              </div>
+              <div className="px-3">
+                <p className="text-lg text-[#616161] font-[700] my-2">
+                  Population:{" "}
+                  <span className="text-[#747474]">
+                    {country.population.toLocaleString()}
+                  </span>
+                </p>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
+
+      <div>
+        <button
+          className="bg-green-500 rounded fixed right-3 bottom-3 p-2"
+          onClick={scrolltrigger}
+        >
+          <FaArrowUp size={24} />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default Allcountrydata;
-
-
--- src/Components/Navbr.jsx
+++ src/Components/Navbr.jsx
@@ -1,42 +1,338 @@
-import React from 'react'
+// import React, { useState } from "react";
 
-function Navbr() {
+// function Navbr() {
+//   const [isOpen, setIsOpen] = useState(false);
+
+//   const handleToggle = () => {
+//     setIsOpen(!isOpen);
+//   };
+
+//   return (
+//     <div>
+//       <nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-lg h-20 py-2 ">
+//         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+//           <a
+//             href="https://flowbite.com/"
+//             className="flex items-center space-x-3 rtl:space-x-reverse"
+//           >
+//             <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white underline">
+//               {"30DOR"}
+//             </span>
+//           </a>
+//           <button
+//             onClick={handleToggle}
+//             type="button"
+//             className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+//             aria-controls="navbar-default"
+//             aria-expanded={isOpen ? "true" : "false"}
+//           >
+//             <svg
+//               className="w-5 h-5"
+//               aria-hidden="true"
+//               xmlns="http://www.w3.org/2000/svg"
+//               fill="none"
+//               viewBox="0 0 17 14"
+//             >
+//               <path
+//                 stroke="currentColor"
+//                 strokeLinecap="round"
+//                 strokeLinejoin="round"
+//                 strokeWidth="2"
+//                 d="M1 1h15M1 7h15M1 13h15"
+//               />
+//             </svg>
+//           </button>
+//           <div
+//             className={`${
+//               isOpen ? "block" : "hidden"
+//             } w-full md:block md:w-auto`}
+//             id="navbar-default"
+//           >
+//             <ul className="text-[20px] flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+//               <li>
+//                 <a
+//                   href=""
+//                   className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
+//                   aria-current="page"
+//                 >
+//                   Courses
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   About
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Login
+//                 </a>
+//               </li>
+//               <li>
+//                 <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Register
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// }
+
+// export default Navbr;
+
+// import React, { useState } from 'react';
+// import { FaLongArrowAltRight } from "react-icons/fa";
+
+// function Navbar() {
+//   const [sidebarOpen, setSidebarOpen] = useState(false);
+
+//   const toggleSidebar = () => {
+//     setSidebarOpen(!sidebarOpen);
+//   };
+
+ 
+
+//   return (
+//     <>
+//       <nav className=" sticky top-0 left-0 bg-white border-gray-200 dark:bg-gray-900 shadow-lg h-20 py-2">
+//         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+//           <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+//             <span className="self-center text-2xl text-[#ffffff] whitespace-nowrap ">THE MOVIX</span>
+//           </a>
+//           <button
+//             type="button"
+//             className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+//             onClick={toggleSidebar}
+//             aria-controls="navbar"
+//             aria-expanded="false"
+//           >
+//             <span className="sr-only">Open main menu</span>
+//             <svg
+//               className="w-6 h-6"
+//               aria-hidden="true"
+//               fill="currentColor"
+//               viewBox="0 0 20 20"
+//               xmlns="http://www.w3.org/2000/svg"
+//             >
+//               <path
+//                 fillRule="evenodd"
+//                 d="M3 5a1 1 0 012 0h10a1 1 0 110 2H5a1 1 0 01-2 0zM3 10a1 1 0 012 0h10a1 1 0 110 2H5a1 1 0 01-2 0zM3 15a1 1 0 012 0h10a1 1 0 110 2H5a1 1 0 01-2 0z"
+//                 clipRule="evenodd"
+//               ></path>
+//             </svg>
+//           </button>
+//           <div className="hidden w-full md:block md:w-auto" id="navbar">
+//             <ul className="flex flex-col p-4 md:p-0 mt-4 border  rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+//               <li>
+//               <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Register
+//                 </a>
+//               </li>
+//               <li>
+//               <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Register
+//                 </a>
+//               </li>
+//               <li>
+//               <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Register
+//                 </a>
+//               </li>
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+
+//       <div
+//         className={`fixed top-0 left-0 w-72 h-full bg-[#032541] z-10 shadow-lg transform md:hidden transition-transform ${
+//           sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+//         }`}
+//       >
+//         <div className='text-white w-12 mt-5 ml-52 cursor-pointer'>
+//           <FaLongArrowAltRight size={50} onClick={toggleSidebar}/>
+//         </div>
+
+//         <ul className="mt-2 space-y-2">
+//           <li>
+//           <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-wh"
+//                 >
+//                   Register
+//                 </a>
+//           </li>
+//           <li>
+//           <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Register
+//                 </a>
+//           </li>
+//           <li>
+//           <a
+//                   href=""
+//                   className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+//                 >
+//                   Register
+//                 </a>
+            
+//           </li>
+//         </ul>
+//       </div>
+//     </>
+//   );
+// }
+
+// export default Navbar;
+
+
+import React, { useState } from 'react';
+import { FaLongArrowAltRight } from "react-icons/fa";
+
+function Navbar() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
-    <div>
-<nav className="bg-white border-gray-200 dark:bg-gray-900 shadow-lg h-20 py-2">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a href="https://flowbite.com/" className="flex items-center space-x-3 rtl:space-x-reverse">
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white underline">{'30DOR'}</span>
-    </a>
-    <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
-        </svg>
-    </button>
-    <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-      <ul className="text-[20px] flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a href="" className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">Courses</a>
-        </li>
-        <li>
-          <a href="" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">About</a>
-        </li>
-       
-        <li>
-          <a href="" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Login</a>
-        </li>
-        <li>
-          <a href="" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Register</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-    </div>
-  )
+    <>
+      <nav className="sticky top-0 left-0 bg-white border-gray-200 dark:bg-gray-900 shadow-lg h-20 py-2 z-20">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <a href="" className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span className="self-center text-2xl text-[black] whitespace-nowrap ">  {"30DOR"}</span>
+          </a>
+          <button
+            type="button"
+            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            onClick={toggleSidebar}
+            aria-controls="navbar"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-6 h-6"
+              aria-hidden="true"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M3 5a1 1 0 012 0h10a1 1 0 110 2H5a1 1 0 01-2 0zM3 10a1 1 0 012 0h10a1 1 0 110 2H5a1 1 0 01-2 0zM3 15a1 1 0 012 0h10a1 1 0 110 2H5a1 1 0 01-2 0z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </button>
+          <div className="hidden w-full md:block md:w-auto" id="navbar">
+            <ul className="flex flex-col p-4 md:p-0 mt-4 border rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+              <li>
+                <a
+                  href=""
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Home
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Services
+                </a>
+              </li>
+              <li>
+                <a
+                  href=""
+                  className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div
+        className={`fixed top-0 left-0 w-72 h-full bg-white z-10 shadow-lg transform md:hidden transition-transform ${
+          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+        }`}
+      >
+        <div className='text-white w-12 mt-5 ml-52 cursor-pointer'>
+          <FaLongArrowAltRight size={50} onClick={toggleSidebar}/>
+        </div>
+
+        <ul className="mt-2 space-y-2">
+          <li>
+            <a
+              href=""
+              className="block py-2 px-3 text-black rounded mt-10 "
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Services
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white"
+            >
+              Contact
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
-export default Navbr
-
+export default Navbar;
 
